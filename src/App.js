df644fde0495d2e85b845874605f40c5c30a163b
Samantha Noussi <nsamanthaduclaire@gmail.com>
2021-12-27 10:43:10 +0100

    index designed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <diV className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea className='form-control' rows='35'>
+          </textarea>
+        </div>
+      <div className='col-sm-6'>
+       <h1>
+         Résultat
+       </h1>
+      </div>
+      </div>
+    </diV>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
