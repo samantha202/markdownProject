2689c1dfb9fd0ac9859f783b95c42bd6f976658f
Samantha Noussi <nsamanthaduclaire@gmail.com>
2021-12-27 12:05:47 +0100

    markdown with marked.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
+import { Component } from 'react/cjs/react.production.min';
 import './App.css';
+import {sampleText}  from './sampleText';
+import { marked } from 'marked';
 
-function App() {
-  return (
-    <diV className="container">
-      <div className="row">
-        <div className="col-sm-6">
-          <textarea className='form-control' rows='35'>
-          </textarea>
+class App extends Component{
+  state = {
+    text: sampleText
+  }
+  handleChange = event =>{
+    const text = event.target.value
+    this.setState({text})
+  }
+  renderText = text =>{
+   const __html = marked(text, {sanitize: true})
+   return {__html}
+  }
+  render () {
+    return (
+      <diV className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea className='form-control' rows='35' value={this.state.text} onChange= {this.handleChange}>
+            </textarea>
+          </div>
+        <div className='col-sm-6'>
+          <div dangerouslySetInnerHTML={this.renderText(this.state.text)}></div>
         </div>
-      <div className='col-sm-6'>
-       <h1>
-         Résultat
-       </h1>
-      </div>
-      </div>
-    </diV>
-  );
+        </div>
+      </diV>
+    );
+  }
 }
 
 export default App;
